8a15ffc5c35abc73e545a221d80dfa00b56f4317
Dr Andy <andy@scientio.com>
2023-10-01 22:36:01 +0100

    Update ProposalView.cs

--- Models/ProposalView.cs
+++ Models/ProposalView.cs
@@ -2,7 +2,7 @@
 {
     public class ProposalView
     {
-        public enum Status { open, succeeded, failed, executed }
+        public enum Status { open, succeeded, failed, executed, pending }
         public string title { get; set; }
         public string summary { get; set; }
         public List<Document> resources { get; set; }
